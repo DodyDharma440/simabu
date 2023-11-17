c4df8cd1344e667406a086a1dd5d86daf2adf3b6
Dodi Aditya <dodiaridharma@gmail.com>
2023-11-17 12:41:25 +0800

    move api url to env

--- src/common/configs/api.ts
+++ src/common/configs/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiSimabu = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${process.env["NEXT_PUBLIC_API_BASE_URL"]}/api`,
   withCredentials: true,
 });
