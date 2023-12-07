c037e2d204b30307dfb955ee0bd8b1ec32a82f21
Dodi Aditya <dodiaridharma@gmail.com>
2023-12-07 10:29:46 +0800

    navbar layout landing page

---
+++ src/modules/landing-page/components/Navbar/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import { Box, Button, Group, Title } from "@mantine/core";
+import { isDark } from "@/common/utils/theme";
+
+const Navbar = () => {
+  return (
+    <Box
+      component="nav"
+      sx={(theme) => ({
+        position: "fixed",
+        width: `calc(${theme.breakpoints.xs} - 2px)`,
+        boxShadow: theme.shadows.md,
+        backgroundColor: isDark(theme) ? theme.colors.dark[8] : theme.white,
+      })}
+      px="md"
+      py="sm"
+      mx="auto"
+    >
+      <Group position="apart">
+        <Title order={3}>Simabu</Title>
+        <Link href="/login">
+          <Button>Login</Button>
+        </Link>
+      </Group>
+    </Box>
+  );
+};
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import { Button } from "@mantine/core";
-import Link from "next/link";
+import { Loader } from "@/common/components";
 import MobileLayout from "@/common/layouts/MobileLayout";
+import { Navbar } from "@/landing-page/components";
+import { useUserProfile } from "@/auth/hooks";
 
 const Simabu = () => {
+  const { replace } = useRouter();
+  const { userData } = useUserProfile();
+
+  const isAdmin = useMemo(() => {
+    return ["Admin", "Petugas"].includes(userData?.user?.role?.name || "");
+  }, [userData?.user?.role?.name]);
+
+  useEffect(() => {
+    if (isAdmin) {
+      replace("/admin/dashboard");
+    }
+  }, [isAdmin, replace]);
+
   return (
     <>
       <Head>
         <title>SIMABU</title>
       </Head>
 
-      <MobileLayout>
-        <Link href="/login">
-          <Button>Login</Button>
-        </Link>
-      </MobileLayout>
+      {isAdmin ? (
+        <Loader isLoading screenLoader>
+          <></>
+        </Loader>
+      ) : (
+        <MobileLayout>
+          <Navbar />
+        </MobileLayout>
+      )}
     </>
   );
 };
